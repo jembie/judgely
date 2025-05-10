79bd4d19fb06f61a3f8eb20ae4b2e110092f24a7
jembie <75253705+jembie@users.noreply.github.com>
2025-05-10 16:55:56 +0200

    Update `main.py` and `uv.lock` + `pyproject.toml`

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from src.judge import Judge
+from utils.startup import launch
+
+
 def main():
-    print("Hello from judgely!")
+    launch()
+    message = [{"role": "user", "content": "Why is the sky blue?"}]
+
+    judge = Judge()
+    reply = judge.judge(messages=message)
+    print(reply)
 
 
 if __name__ == "__main__":
