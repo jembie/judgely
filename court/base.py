3ebd8e7788a06f51ce5420465f019c13319a16de
jembie <75253705+jembie@users.noreply.github.com>
2025-05-26 10:35:50 +0200

    Revert SingleTon

--- court/base.py
+++ court/base.py
@@ -4,12 +4,9 @@ from abc import ABC, abstractmethod
 
 
 class BaseModel(ABC):
-    _client_instance = None
 
     def __init__(self):
-        if BaseModel._client_instance is None:
-            BaseModel._client_instance = Client()
-        self.client = BaseModel._client_instance
+        self.client = Client()
 
     @abstractmethod
     def chat(self, messages: List[Dict]) -> ChatResponse: ...
