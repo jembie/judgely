a9006e650b7027dd9fb68367b472da308e793122
jembie <75253705+jembie@users.noreply.github.com>
2025-06-05 13:57:30 +0200

    Update `court/judge` system prompt

--- court/judge.py
+++ court/judge.py
@@ -1,14 +1,25 @@
 from typing import Dict, List, Optional
 from ollama import ChatResponse
 from .base import BaseModel
+import pandas as pd
 
 
 class Judge(BaseModel):
 
-    def __init__(self, model: Optional[str] = "deepseek-r1:1.5b", system_message: Optional[str] = "You are an expert clinical doctor."):
+    def __init__(
+        self,
+        model: Optional[str] = "deepseek-r1-1.5b-max",
+        system_message: Optional[
+            str
+        ] = """
+        You are an expert clinical doctor.
+        Your task is to determine whether the following answers (tagged with [START INPUT] for showcasing the beginning of an section to evaluate and [END INPUT] for demonstrating the end of the section to be evaluated.) match the [QUESTION] and [ANSWER TO QUESTION] sections.
+        Give a rating of confidence to the [INPUT]s a the end.
+        """,
+    ):
         super().__init__(model, system_message)
 
-    def chat(self, messages: List[Dict]) -> str | None:
+    def chat(self, messages: List[Dict]) -> str:
         full_message = [self.system_message_dict] + messages
 
         response: ChatResponse = self.client.chat(self.model, messages=full_message)
