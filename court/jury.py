d373e32cee29dce12313916d95a5530a34a6f8aa
jembie <75253705+jembie@users.noreply.github.com>
2025-06-05 13:57:54 +0200

    Uodate `chat()` method for `court/jury`

--- court/jury.py
+++ court/jury.py
@@ -6,9 +6,17 @@ from .base import BaseModel
 # TODO: Implement/Update jury's role
 class Jury(BaseModel):
 
-    def __init__(self, model: Optional[str] = "deepseek-r1:1.5b", system_message: Optional[str] = "You are an expert clinical doctor."):
+    def __init__(self, model: Optional[str] = "deepseek-r1-1.5b-max", system_message: Optional[str] = "You are an expert clinical doctor."):
         super().__init__(model, system_message)
+
+    def format_response(self, response: str) -> str:
+        cut_think_tags = response.split("</think>")[1]
+        formatted_input = "[START INPUT]" + cut_think_tags + "\n\n[END INPUT]"
+
+        return formatted_input
 
     def chat(self, messages: List[Dict]) -> str | None:
         response: ChatResponse = self.client.chat(self.model, messages=messages)
-        return response.message.content
+        formatted_response = self.format_response(response=response.message.content)
+
+        return formatted_response or ""
