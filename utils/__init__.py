d551ce7610cca9fe18952df49757eafb7f6af2b9
jembie <75253705+jembie@users.noreply.github.com>
2025-06-05 13:56:32 +0200

    WIP: Add Template Question / Answer Set generations for demonstration - Will Refactor, this is simply for a potential Proof-Of-Concept demonstration.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 from .startup import launch
 from .constants import PID_FILE, OLLAMA_START, BASE_PATH
+from .generators import TestSetGenerator
 
 
-__all__ = ["launch"]
+__all__ = ["launch, TestSetGenerator"]
--- utils/generators.py
+++ utils/generators.py
@@ -2,17 +2,18 @@ import pandas as pd
 from typing import Dict, List
 import numpy as np
 
-from constants import BASE_PATH
+from .constants import BASE_PATH
 from pathlib import Path
 
 
 class TestSetGenerator:
 
-    def __init__(self):
+    def __init__(self, csv_dir_path: Path | str = ""):
         self.csv_dict: Dict[str, pd.DataFrame] = {}
-        self.csv_dir = Path(BASE_PATH, "data", "csv")
+        self.csv_dir = csv_dir_path if csv_dir_path else Path(BASE_PATH, "data", "csv")
+        self._load_csvs()
 
-    def load_csvs(self) -> bool:
+    def _load_csvs(self) -> bool:
         csv_files = [path for path in self.csv_dir.glob("**/*") if path.is_file()]
 
         if not csv_files:
@@ -24,7 +25,7 @@ class TestSetGenerator:
             self.csv_dict[dataset_name] = pd.read_csv(csv)
             return True
 
-    def generate_set(self, seed: int = 42, amount: int = 10) -> pd.DataFrame:
+    def generate_set(self, seed: int = 42, amount: int = 10) -> Dict[str, pd.DataFrame]:
         rng = np.random.default_rng(seed=seed)
 
         results = {}
@@ -37,10 +38,40 @@ class TestSetGenerator:
 
         return results
 
+    def generate_questions(self) -> List[Dict]:
+        question_set = self.generate_set(amount=3)
+        question_template = {"role": "user", "content": ""}
 
-if __name__ == "__main__":
+        results = []
+        for _, questions_df in question_set.items():
+            for row in questions_df.itertuples():
+                template = question_template.copy()
+                question = row.Question
+                # answer = row.Answer
 
-    t = TestSetGenerator()
+                template["content"] = question
+                results.append(template)
 
-    t.load_csvs()
-    t.generate_set()
+        return results
+
+    def create_format(self, question, answer) -> str:
+        format_question = "[QUESTION]" + question + "[QUESTION]"
+        format_answer = "\n[ANSWER TO QUESTION]" + answer + "[ANSWER TO QUESTION]"
+
+        return format_question + format_answer
+
+    def generate_answers(self) -> List[Dict]:
+        question_set = self.generate_set(amount=3)
+        question_template = {"role": "user", "content": ""}
+
+        results = []
+        for _, questions_df in question_set.items():
+            for row in questions_df.itertuples():
+                template = question_template.copy()
+                question = row.Question
+                answer = row.Answer
+
+                template["content"] = self.create_format(question, answer)
+                results.append(template)
+
+        return results
