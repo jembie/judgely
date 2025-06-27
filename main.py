8daabd572bf97d5745843c84ac33b7947cdf26e3
jembie <75253705+jembie@users.noreply.github.com>
2025-06-27 13:30:38 +0200

    Test Config for local running

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from court import Judge, Jury
-from utils import launch, TestSetGenerator, ClientConfig
+from utils import TestSetGenerator, ClientConfig
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BASE_URL = os.environ.get("BASE_URL")
+API_KEY = os.environ.get("API_KEY")
+
+
+def run():
+    config = ClientConfig(BASE_URL, API_KEY)
+    # generator = TestSetGenerator()
+    # questions = generator.generate_questions(amount=1)
+    question_template = {"role": "user", "content": "What is the point of points?"}
+
+    # launch()
+    jury = Jury(model="llama-3.1-70b-instruct-q4km", client_config=config)
+
+    reply = jury.chat(messages=question_template)
+    print(reply)
 
 
 def main():
-    generator = TestSetGenerator()
-    questions = generator.generate_questions()
     # answers = generator.generate_answers()
-
-    launch()
-
-    # judge = Judge()
-    jury = Jury()
-
-    reply = jury.chat(messages=questions)
-    # messages = answers + [{"role": "user", "content": "[START INPUT]" + reply + "[END INPUT]"}]
-    # rep = judge.chat(messages=messages)
-
-    print(reply)
+    run()
 
 
 if __name__ == "__main__":
