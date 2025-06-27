3270b470a0d8cbd7df7c0c95011e556c9f45c591
jembie <75253705+jembie@users.noreply.github.com>
2025-06-27 12:20:21 +0200

    Add default systemprompt for `court/judge.py` and also Optional `client_config`

--- court/judge.py
+++ court/judge.py
@@ -2,6 +2,7 @@ from typing import Dict, List, Optional
 from openai import OpenAI
 from .base import BaseTemplate
 import pandas as pd
+from utils import ClientConfig
 
 
 class Judge(BaseTemplate):
@@ -11,8 +12,12 @@ class Judge(BaseTemplate):
         model: Optional[str] = "deepseek-r1-1.5b-max",
         system_message: Optional[
             str
-        ] = """
-        You are an expert clinical doctor.
-        """,
+        ] = """You are an assistant that always responds in the following format:
+            - Answer: <YOUR ANSWER>.
+            - Score: a score from 1 to 5.
+            - Reason: a reasoning for your choice of score.
+            """,
+        *,
+        client_config: Optional[ClientConfig] = None,
     ):
         super().__init__(model, system_message)
